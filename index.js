9ce3ada3a94a141b272d5ed6a3fd8f666a2c7a73
bhuvanesh-123 <bhuvangowri44@gmail.com>
2025-03-07 22:59:31 +0530

    Addfiles

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-const textTransform = require("./lib/textTransform");
+const { textTransform } = require("./lib/textTransform");
+const { mathFunction } = require("./lib/mathFunction");
 
 textTransform("HeLlO", "lc", (error, data) => {
-  console.log("The transformed string is ", `${data}`);
+  if (error) {
+    console.log("Error:", error);
+  } else {
+    console.log("The transformed string is:", data);
+  }
 });
 
-textTransform.textTransform("hello", "uc", (error, data) => {
-  console.log("The transformed string is ", `${data}`);
+textTransform("hello", "uc", (error, data) => {
+  if (error) {
+    console.log("Error:", error);
+  } else {
+    console.log("The transformed string is:", data);
+  }
+});
+
+mathFunction(10, 10, "add", (err, data) => {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    console.log(`The add result is: ${data}`);
+  }
+});
+mathFunction(10, 10, "subtract", (err, data) => {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    console.log(`The subtract result is: ${data}`);
+  }
+});
+mathFunction(10, 10, "divide", (err, data) => {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    console.log(`The division result result is: ${data}`);
+  }
+});
+mathFunction(10, 10, "multiply", (err, data) => {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    console.log(`The multiply result is: ${data}`);
+  }
 });
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const http = require("http");
-const { mainRoute } = require("./lib/mainRoute.js");
+const { mainRoute } = require("./lib/mainRoute");
+
 const server = http.createServer((req, res) => {
-  mainRoute(req.url, req.method, (err, data) => {
+  mainRoute(req.url, req.method, function (err, data) {
     if (err) {
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Internal Server Error");
       return;
     }
-
-    const { status, contentType, responseData } = data;
-    res.writeHead(status, { "Content-Type": contentType });
+    var response = data;
+    const { status, contentType, responseData } = response;
+    res.writeHead(status, { "content-type": contentType });
     res.end(responseData);
   });
 });
-
-server.listen(8000, () => {
-  console.log("Server is running on port 8000");
+server.listen(3000, () => {
+  console.log("The server is running in the port 3000");
 });
